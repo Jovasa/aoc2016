790760d7775852f17fcc12bc45a467ce26dc5644
Jovasa <jovasa46@gmail.com>
2022-08-25 21:45:41 +0300

    day11 second if you just wait couple of minutes

--- src/day11.rs
+++ src/day11.rs
@@ -1,25 +1,27 @@
 use std::collections::HashSet;
 
-const GENERATOR_OFFSET: u64 = 5;
-const CHIPS: u64 = (1 << GENERATOR_OFFSET) - 1;
-const FLOOR_OFFSET: u64 = GENERATOR_OFFSET * 2;
-const FLOOR: u64 = (1 << FLOOR_OFFSET) - 1;
+const GENERATOR_OFFSET: u128 = 7;
+const CHIPS: u128 = (1 << GENERATOR_OFFSET) - 1;
+const FLOOR_OFFSET: u128 = GENERATOR_OFFSET * 2;
+const FLOOR: u128 = (1 << FLOOR_OFFSET) - 1;
 
-const POLONIUM: u64 = 0;
-const THULIUM: u64 = 1;
-const PROMETHIUM: u64 = 2;
-const RUTHENIUM: u64 = 3;
-const COBALT: u64 = 4;
+const POLONIUM: u128 = 0;
+const THULIUM: u128 = 1;
+const PROMETHIUM: u128 = 2;
+const RUTHENIUM: u128 = 3;
+const COBALT: u128 = 4;
+const ELERIUM: u128 = 5;
+const DILITHIUM: u128 = 6;
 
-const FLOOR1: u64 = FLOOR_OFFSET * 0;
-const FLOOR2: u64 = FLOOR_OFFSET * 1;
-const FLOOR3: u64 = FLOOR_OFFSET * 2;
-const FLOOR4: u64 = FLOOR_OFFSET * 3;
+const FLOOR1: u128 = FLOOR_OFFSET * 0;
+const FLOOR2: u128 = FLOOR_OFFSET * 1;
+const FLOOR3: u128 = FLOOR_OFFSET * 2;
+const FLOOR4: u128 = FLOOR_OFFSET * 3;
 
-const CURRENT_FLOOR_OFFSET: u64 = 60;
+const CURRENT_FLOOR_OFFSET: u128 = 100;
 
 
-fn is_valid_floor(in_: u64) -> bool {
+fn is_valid_floor(in_: u128) -> bool {
     let both = in_ & (in_ >> GENERATOR_OFFSET);
     if both == 0 {
         return true;
@@ -27,14 +29,14 @@ fn is_valid_floor(in_: u64) -> bool {
     (both ^ (in_ & CHIPS)) == 0
 }
 
-fn check_all_floors(in_: u64) -> bool {
+fn check_all_floors(in_: u128) -> bool {
     (0..4)
         .map(|x| is_valid_floor((in_ >> (FLOOR_OFFSET * x)) & FLOOR))
         .fold(true, |a, p| a && p)
 }
 
 fn main() {
-    let start: u64 = 1 << (POLONIUM + GENERATOR_OFFSET + FLOOR1) |
+    let start: u128 = 1 << (POLONIUM + GENERATOR_OFFSET + FLOOR1) |
         1 << (POLONIUM + FLOOR2) |
         1 << (THULIUM + GENERATOR_OFFSET + FLOOR1) |
         1 << (THULIUM + FLOOR1) |
@@ -44,6 +46,10 @@ fn main() {
         1 << (RUTHENIUM + FLOOR1) |
         1 << (COBALT + GENERATOR_OFFSET + FLOOR1) |
         1 << (COBALT + FLOOR1) |
+        1 << (ELERIUM + GENERATOR_OFFSET + FLOOR1) |
+        1 << (ELERIUM + FLOOR1) |
+        1 << (DILITHIUM + GENERATOR_OFFSET + FLOOR1) |
+        1 << (DILITHIUM + FLOOR1) |
         0 << CURRENT_FLOOR_OFFSET;
 
     let mut iters = 0;
@@ -57,12 +63,12 @@ fn main() {
         let mut temp = HashSet::new();
         for item in current_set {
             let current_floor = item >> CURRENT_FLOOR_OFFSET;
-            for chip in 0..5 {
+            for chip in 0..GENERATOR_OFFSET {
                 let c = item & (1 << (chip + current_floor * FLOOR_OFFSET));
                 if c != 0 {
                     let current_chip_removed = item ^ c ^ (current_floor << CURRENT_FLOOR_OFFSET);
 
-                    for obj in 0..10 {
+                    for obj in 0..FLOOR_OFFSET {
                         let o = current_chip_removed & (1 << (obj + current_floor * FLOOR_OFFSET));
                         if o != 0 || chip == obj {
                             let sec_obj_removed = current_chip_removed ^ o;
