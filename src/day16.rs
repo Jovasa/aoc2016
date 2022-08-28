4fa2bcbb7fb2437b263d378fa16847a2254e2acc
Jovasa <jovasa46@gmail.com>
2022-08-28 20:42:16 +0300

    Day16 so much faster

--- src/day16.rs
+++ src/day16.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 fn extend(s: &str) -> String {
     let a = s.to_string();
     let b = s.chars().rev().map(|x| match x { '0' => '1', '1'=>'0', _ => unreachable!() }).collect::<String>();
@@ -6,19 +8,17 @@ fn extend(s: &str) -> String {
 
 fn main() {
     let mut start = "01000100010010111".to_string();
-    while start.len() < 272 {
+    while start.len() < 35651584 {
         start = extend(&start);
     }
-    start = start.split_at(272).0.to_string();
-    let mut checksum = start;
+    start = start.split_at(35651584).0.to_string();
+    let mut checksum = start.chars().map(|x| match x { '1' => true, _ => false }).collect_vec();
     while checksum.len() % 2 == 0 {
-        let mut temp = Vec::new();
-        for i in 0..checksum.len()/2 {
-            let t = checksum.chars().nth(i*2).unwrap();
-            let t2 = checksum.chars().nth(i*2 + 1).unwrap();
-            if t == t2 {temp.push('1')} else { temp.push('0') }
-        }
-        checksum = temp.iter().collect();
+        checksum = checksum.iter().tuple_windows().step_by(2).map(|(x, y)| x == y).collect_vec();
     }
-    println!("{}", checksum);
+    for i in checksum.iter() {
+        let v = if *i {'1'} else {'0'};
+        print!("{}", v);
+    }
+    println!()
 }
