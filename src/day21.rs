1a0addd3143d87fe7aab5ca7ea3603cdc70e68f0
Jovasa <jovasa46@gmail.com>
2022-08-30 17:04:50 +0300

    Day 21 second

--- src/day21.rs
+++ src/day21.rs
@@ -3,12 +3,14 @@ use std::io::{BufRead, BufReader};
 use itertools::Itertools;
 
 fn main() {
-    let mut pw = "abcdefgh".chars().collect_vec();
-    //let mut pw = "fbgdceah".chars().collect_vec();
+    // let mut pw = "gfdhebac".chars().collect_vec();
+    let mut pw = "fbgdceah".chars().collect_vec();
+
+    let reverse_rotations = [1, 1, 6, 2, 7, 3, 0, 4];
 
     let reader = BufReader::new(File::open("data/day21.txt").unwrap());
     let lines = reader.lines().map(|x| x.unwrap()).collect_vec();
-    for line in lines.iter() {
+    for line in lines.iter().rev() {
         let parts = line.split(" ").collect_vec();
         match parts[0] {
             "reverse" => {
@@ -25,31 +27,33 @@ fn main() {
                 match parts[1] {
                     "left" => {
                         let amount = parts[2].parse::<usize>().unwrap();
-                        pw.rotate_left(amount);
+                        pw.rotate_right(amount);
                     },
                     "right" => {
                         let amount = parts[2].parse::<usize>().unwrap();
-                        pw.rotate_right(amount);
+                        pw.rotate_left(amount);
                     },
                     "based" => {
-                        let mut amount = pw.
+                        let position = pw.
                             iter()
                             .position(|x| x == &parts[6]
                                 .chars()
                                 .nth(0)
                                 .unwrap())
-                            .unwrap() + 1;
-                        if amount > 4 {amount += 1}
+                            .unwrap();
+                        //let mut amount = position + 1;
+                        //if amount > 4 {amount += 1}
+                        let amount = reverse_rotations[position];
 
                         let len = pw.len();
-                        pw.rotate_right(amount % len);
+                        pw.rotate_left(amount % len);
                     },
                     _ => unreachable!()
                 }
             },
             "move" => {
-                let start = parts[2].parse::<usize>().unwrap();
-                let end = parts[5].parse::<usize>().unwrap();
+                let start = parts[5].parse::<usize>().unwrap();
+                let end = parts[2].parse::<usize>().unwrap();
                 let t = pw.remove(start);
                 pw.insert(end, t);
             },
