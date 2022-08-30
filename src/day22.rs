b4821fe9333232c8aded69a373b3b28baca0a160
Jovasa <jovasa46@gmail.com>
2022-08-30 21:48:21 +0300

    Day 22 Second

--- src/day22.rs
+++ src/day22.rs
@@ -25,6 +25,12 @@ fn main() {
     let mut count = 0;
 
     for (i, node) in nodes.iter().enumerate() {
+        if node[2] > 100 {
+            println!("{} {}", node[0], node[1]);
+        }
+        if node[3] == 0 {
+            println!("Zero {} {}", node[0], node[1]);
+        }
         for j in i+1..nodes.len() {
             let temp = &nodes[j];
             if temp[3] != 0 && temp[3] < node[4] {
@@ -37,4 +43,10 @@ fn main() {
     }
 
     println!("{}", count);
+
+    // for the second part it's faster for me to do math on how many moves needs to be
+    // done based on the locations.
+    // The empty spot needs to move to all the way left (12) then all the way up (14)
+    // all the way right (32) and then it is 5 moves for each move of the goal for 31 times
+    // So in total 12+14+32+5*31 = 213
 }
