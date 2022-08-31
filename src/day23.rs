c3c3d665eba1fbed8fdffbb08f585c3a9b6280a3
Jovasa <jovasa46@gmail.com>
2022-08-31 18:35:44 +0300

    Day 23

--- src/day23.rs
+++ src/day23.rs
@@ -1,13 +1,17 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use itertools::Itertools;
-use crate::OpCode::{COPY, DEC, INC, JUMP};
+use crate::OpCode::{COPY, DEC, INC, JUMP, TOGGLE};
+use crate::day12::{parse_register, parse_value};
+
+mod day12;
 
 enum OpCode {
     COPY,
     JUMP,
     INC,
-    DEC
+    DEC,
+    TOGGLE
 }
 
 
@@ -19,31 +23,14 @@ struct Op {
     second_val: Option<i32>,
 }
 
-pub fn parse_register(i: &str) -> Option<usize> {
-    if i.len() == 1 {
-        let temp = i.chars().next().unwrap();
-        if temp.is_alphabetic() {
-            let a = 'a' as usize;
-            return Some(temp as usize - a);
-        }
-    }
-    None
-}
-
-pub fn parse_value(i: &str) -> Option<i32> {
-    match i.parse::<i32>() {
-        Ok(i) => Some(i),
-        Err(_) => None
-    }
-}
 
 
 fn main() {
-    let reader = BufReader::new(File::open("data/day12.txt").unwrap());
+    let reader = BufReader::new(File::open("data/day23.txt").unwrap());
     let mut program = Vec::new();
     let mut register = [0; 4];
 
-    register[2] = 1;
+    register[0] = 12;
 
     for line in reader.lines() {
         let line = line.unwrap();
@@ -51,69 +38,101 @@ fn main() {
         program.push(
             match parts[0] {
                 "cpy" => Op{
-                    opcode: OpCode::COPY,
+                    opcode: COPY,
                     first_reg: parse_register(parts[1]),
                     first_val: parse_value(parts[1]),
                     second_reg: parse_register(parts[2]),
                     second_val: parse_value(parts[2]),
                 },
                 "jnz" => Op{
-                    opcode: OpCode::JUMP,
+                    opcode: JUMP,
                     first_reg: parse_register(parts[1]),
                     first_val: parse_value(parts[1]),
                     second_reg: parse_register(parts[2]),
                     second_val: parse_value(parts[2]),
                 },
                 "inc"=> Op{
-                    opcode: OpCode::INC,
+                    opcode: INC,
                     first_reg: parse_register(parts[1]),
                     first_val: parse_value(parts[1]),
                     second_reg: None,
                     second_val: None,
                 },
                 "dec"=> Op{
-                    opcode: OpCode::DEC,
+                    opcode: DEC,
                     first_reg: parse_register(parts[1]),
                     first_val: parse_value(parts[1]),
                     second_reg: None,
                     second_val: None,
                 },
+                "tgl" => {
+                    Op {
+                        opcode: TOGGLE,
+                        first_reg: parse_register(parts[1]),
+                        first_val: None,
+                        second_reg: None,
+                        second_val: None,
+                    }
+                }
                 _ => unreachable!()
             }
         )
     }
 
-    let mut pc: i32 = 0;
+    let mut pc: usize = 0;
 
     while (pc as usize) < program.len() {
         let temp = &program[pc as usize];
         match temp.opcode {
-            OpCode::COPY => {
+            COPY => {
                 let value = match temp.first_val {
                     Some(i) => i,
                     None => register[temp.first_reg.unwrap()]
                 };
+                if temp.second_val.is_some() { continue }
                 register[temp.second_reg.unwrap()] = value;
                 pc += 1;
             }
-            OpCode::JUMP => {
+            JUMP => {
                 let value = match temp.first_val {
                     Some(i) => i,
                     None => register[temp.first_reg.unwrap()]
                 };
                 if value != 0 {
-                    pc += temp.second_val.unwrap();
+                    let jmp = match temp.second_val {
+                        Some(i) => i,
+                        None => register[temp.second_reg.unwrap()]
+                    };
+                    pc = (pc as i64 + jmp as i64) as usize;
                 }
                 else {
                     pc += 1;
                 }
             }
-            OpCode::INC => {
+            INC => {
+                if temp.first_val.is_some() {continue };
                 register[temp.first_reg.unwrap()] += 1;
                 pc += 1;
             },
-            OpCode::DEC => {
+            DEC => {
+                if temp.first_val.is_some() {continue };
                 register[temp.first_reg.unwrap()] -= 1 ;
+                pc += 1;
+            },
+            TOGGLE => {
+                let value = pc + register[temp.first_reg.unwrap()] as usize;
+                if value >= program.len() {
+                    pc += 1;
+                    continue
+                }
+                let op = &program[value].opcode;
+                match op {
+                    INC => program[value].opcode = DEC,
+                    DEC => program[value].opcode = INC,
+                    JUMP => program[value].opcode = COPY,
+                    COPY => program[value].opcode = JUMP,
+                    TOGGLE => program[value].opcode = INC,
+                }
                 pc += 1;
             }
         }
